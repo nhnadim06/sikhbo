ecccb8764748a850ffb6a860e8a40459757b258c
Nadim <nhnadim06@gmail.com>
2024-10-07 13:15:24 +0600

    template update

--- base/views.py
+++ base/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request,"home.html")
+
+def signup(request):
+    return render (request,"signup.html")
+
+def login(request):
+    return render (request,"login.html")  
+
+def courses(request):
+    return render (request,"courses.html")  
--- eLearning/urls.py
+++ eLearning/urls.py
@@ -20,4 +20,7 @@ from base import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name="home"),
+    path('signup/', views.signup,name="signup"),
+    path('login/', views.login,name="login"),
+    path('courses/', views.courses,name="courses"),
 ]
